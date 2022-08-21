72d1a5dca1115a4451a5e28147ddea2da9275add
pramod neo <pramodbruno@gmail.com>
2022-08-22 00:41:08 +0530

    king 2

--- server.js
+++ server.js
@@ -224,4 +224,4 @@ async function commitDb(promise) {
         return data
 }
 
-app .listen({port: process.env.PORT || 3000, host: '0.0.0.0'})
+app .listen({port: process.env.PORT || '3000', host: process.env.HOST || '0.0.0.0'})
