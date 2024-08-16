b523fb11781835cfcf79051fba3b549d263e4c42
Prati-source <60906673+Prati-source@users.noreply.github.com>
2024-08-16 19:46:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cookie from "@fastify/cookie";
 import CryptoJS  from "crypto-js";
 import cors from "@fastify/cors";
-import { PrismaClient } from "./prisma/generated/client";
+import { PrismaClient } from "./prisma/generated/client/index.js";
 dotenv.config();
 
 
