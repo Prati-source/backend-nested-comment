470d82573a2a7f04e4f5202344ebe5ed64044c46
Prati-Source <pramodbruno@gmail.com>
2024-08-17 21:18:32 +0530

    login cookies error

--- api/index.js
+++ api/index.js
@@ -367,8 +367,8 @@ app.post("/login", async (req, res, done) => {
         }))
         console.log(User.id)
         if(User.password === CryptoJS.SHA256(req.body.password).toString()){
-            res.setCookie("userId", User.id)
-            res.setCookie("name",User.name)
+            req.setCookie("userId", User.id)
+            req.setCookie("name",User.name)
            return res.send(User)
            
                    
