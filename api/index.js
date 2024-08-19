c2966b5aa5a4a2398df1540f95623da26a404269
Prati-Source <pramodbruno@gmail.com>
2024-08-19 22:01:01 +0530

    clear cookies

--- api/index.js
+++ api/index.js
@@ -363,6 +363,8 @@ app.post("/login", async (req, res, done) => {
         }))
         console.log(User.id)
         if(User.password === CryptoJS.SHA256(req.body.password).toString()){
+            req.cookies.userId=null
+            req.cookies.name=null
             res.setCookie("userId", User.id)
             res.setCookie("name",User.name)
            return res.send(User)
